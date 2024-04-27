5682232a63a0a1b61bcbc03d9175db6fd019e566
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-04-27 13:27:09 +0530

    ChangeJquerytoangularjs
    ChangeJquerytoangularjs

---
+++ CrudOperation/Asset/js/directive/decimal-only.js
@@ -0,0 +1,33 @@
+﻿myApp.directive('decimalOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+            if (!ngModelCtrl) return;
+
+            ngModelCtrl.$parsers.push(function (val) {
+                if (angular.isUndefined(val)) {
+                    val = '';
+                }
+                var clean = val.replace(/[^0-9\.]/g, ''); // Allow only digits and decimal point
+                var decimalCheck = clean.split('.');
+
+                if (!angular.isUndefined(decimalCheck[1])) {
+                    decimalCheck[1] = decimalCheck[1].slice(0, 2); // Limit decimal part to 2 digits
+                    clean = decimalCheck[0] + '.' + decimalCheck[1];
+                }
+
+                if (val !== clean) {
+                    ngModelCtrl.$setViewValue(clean);
+                    ngModelCtrl.$render();
+                }
+                return clean;
+            });
+
+            element.bind('keypress', function (event) {
+                if (event.keyCode === 32) {
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+});
