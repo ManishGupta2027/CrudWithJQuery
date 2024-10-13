38cbb0422d962877730ff5852f399a54b3cc82fe
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-10-13 13:00:25 +0530

    Update UpdateBrandModel.cs

--- Crud.Api/Model/Brand/UpdateBrandModel.cs
+++ Crud.Api/Model/Brand/UpdateBrandModel.cs
@@ -6,5 +6,23 @@
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string Description { get; set; }
+        public string LogoPreview { get; set; }
+        public Configuration Flags { get; set; }
+        public ImageModel Images { get; set; }
+        public VideoModel Videos { get; set; }
     }
+    public class Configuration{
+      public bool IsActive { get; set; }
+      public string IsFeatured { get; set; }
+    }
+    public class ImageModel
+    {
+        public string Name { get; set; }
+        public string Base64 { get; set; }
+
+        public int DisplayOrder { get; set; }
+        public string Description { get; set; }
+    }
+    public class VideoModel { }
+ 
 }
