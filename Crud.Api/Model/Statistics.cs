3b50f9c534312bcf254f7563720e695247fb44c3
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-11-30 10:51:19 +0530

    create model

---
+++ Crud.Api/Model/Statistics.cs
@@ -0,0 +1,13 @@
+﻿namespace Crud.Api.Model
+{
+	public class Statistics
+	{
+			public List<ProductStatistics> ProductStatics { get; set; }
+	}
+
+		public class ProductStatistics
+		{
+			public string Key { get; set; }
+			public string Value { get; set; }
+		}
+}
---
+++ CrudOperation/Models/Statistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudOperation.Models
+{
+	public class Statistics
+	{
+		public List<ProductStatics> ProductStatics { get; set; }
+	}
+
+	public class ProductStatics
+	{ 
+	public string Key { get; set; }
+	public string Value { get; set; }
+	}
+
+}
