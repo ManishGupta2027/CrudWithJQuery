f23e6d6cd896ba54df02d15ea921b3d067e8e2f4
ManishGupta <139673041+ManishGupta2027@users.noreply.github.com>
2024-12-05 21:30:43 +0530

    add serilogger
    add serilogger

--- Crud.Api/Controllers/ProductController.cs
+++ Crud.Api/Controllers/ProductController.cs
@@ -18,20 +18,23 @@ namespace Crud.Api.Controllers
 	[ApiController]
 	public class ProductController : ControllerBase
 	{
-		private IProductService _productService;
+        readonly ILogger<ProductController> _logger;
+        private IProductService _productService;
 		private readonly IMapper _mapper;
         private readonly CloudinaryService _cloudinaryService;
-        public ProductController(IProductService productService, IMapper mapper, CloudinaryService cloudinaryService)
+        public ProductController(ILogger<ProductController> logger,IProductService productService, IMapper mapper, CloudinaryService cloudinaryService)
         {
 			_mapper = mapper;
 			_productService = productService;
 			_cloudinaryService = cloudinaryService;
+			_logger = logger;
         }
         // GET: api/<ProductController>
         [HttpGet]
 		public ResponsecPaginationModel<List<ProductListModel>> GetAll(int currentPage ,int pageSize=40, string name=null)
 		{
-			var res = new ResponsecPaginationModel<List<ProductListModel>>();
+            _logger.LogInformation("Information level log");
+            var res = new ResponsecPaginationModel<List<ProductListModel>>();
 			var productlist = _productService.GetProductList(currentPage, pageSize,name);
 			var mappedProductList = _mapper.Map<List<ProductListModel>>(productlist);
 			// Populate the response model
@@ -49,7 +52,8 @@ namespace Crud.Api.Controllers
 		[HttpGet("{id}")]
 		public ResponseModel<ProductDetailModel> Get(Guid id)
 		{
-			var response = new ResponseModel<ProductDetailModel>();
+            _logger.LogInformation("Get product id:{@id}");
+            var response = new ResponseModel<ProductDetailModel>();
 			try
 			{
 				var result = _productService.GetProductById(id);
